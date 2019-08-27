2ce5de9400452481605df840c87f6e383ca016d5
Isaac Daniel Batista <disa.indus@gmail.com>
2019-08-26 22:05:16 -0500

    implements permisions with spatie/laravel-permission

--- config/app.php
+++ config/app.php
@@ -160,6 +160,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // Provider for roll
+        Spatie\Permission\PermissionServiceProvider::class,
+
     ],
 
     /*
