aafe707c727ee98e133a2cdb5267c5dde8c12613
Isaac Batista <daniel.bautista@pengostores.com>
2019-11-04 17:04:14 -0600

    update custom login

---
+++ resources/views/layouts/signin.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+  <meta name="description" content="Este es el login de la pagina de opemedios">
+  <meta name="author" content="Isaac Daniel Batista">
+  <!--<link rel="shortcut icon" href="../images/favicon.png" type="image/png">-->
+
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
+  <title>{{ config('app.name', 'Opemedios') }}</title>
+
+  <link rel="stylesheet" href="../lib/fontawesome/css/font-awesome.css">
+
+  <link rel="stylesheet" href="../css/quirk.css">
+
+  <script src="../lib/modernizr/modernizr.js"></script>
+  <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!--[if lt IE 9]>
+  <script src="../lib/html5shiv/html5shiv.js"></script>
+  <script src="../lib/respond/respond.src.js"></script>
+  <![endif]-->
+</head>
+
+<body class="signwrapper">
+
+  <div class="sign-overlay"></div>
+  <div class="signpanel"></div>
+
+  @yield('content')
+
+</body>
+</html>
