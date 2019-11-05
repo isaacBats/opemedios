a4c8fcd59e382436e6c91eea081f56e0b6182315
Isaac Batista <daniel.bautista@pengostores.com>
2019-11-04 23:20:54 -0600

    The model and migration for user metadata was created

---
+++ database/migrations/2019_11_04_230634_create_user_meta_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserMetaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_meta', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+
+            $table->integer('user_id')->unsigned();
+            $table->longText('meta_value');
+            $table->string('meta_key');
+            
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_meta');
+    }
+}
