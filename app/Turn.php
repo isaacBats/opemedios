69bd5fcdf00fd5462ad4d2ee315456b2720352da
Isaac Batista <daniel.bautista@pengostores.com>
2019-11-21 09:37:10 -0600

    Relationship between bussiness and company

--- app/Turn.php
+++ app/Turn.php
@@ -2,9 +2,14 @@
 
 namespace App;
 
+use App\Company;
 use Illuminate\Database\Eloquent\Model;
 
 class Turn extends Model
 {
     protected $fillable = ['name', 'description',];
+
+    public function companies () {
+        return $this->hasMany(Company::class);
+    }
 }
