24d4d9cd81b6588a6e84909709f254089c14e55f
Isaac Batista <disa.indus@gmail.com>
2019-12-24 15:39:46 -0600

    add card for clients

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Company;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
@@ -13,6 +14,8 @@ class AdminController extends Controller
      */
     public function index()
     {
-        return view('admin.home');
+        $count = array();
+        $count['clients'] = Company::all()->count();
+        return view('admin.home', compact('count'));
     }
 }
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,5 +1,29 @@
 @extends('layouts.admin')
 
 @section('content')
-
+    <div class="row">
+        <div class="col-md-9 col-lg-8 dash-left">
+            <div class="panel panel-site-traffic">
+                <div class="panel-heading">
+                    <h4 class="panel-title text-success">Operaciones del día</h4>
+                    {{-- <p class="nomargin">Past 30 Days — Last Updated July 14, 2015</p> --}}
+                </div>
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-xs-6 col-sm-4">
+                            <div class="pull-left">
+                                <div class="icon icon ion-android-people"></div>
+                            </div>
+                            <div class="pull-left">
+                                <h4 class="panel-title">Clientes</h4>
+                                <h3>{{ $count['clients'] }}</h3>
+                                {{-- <h3>23.30%</h3> --}}
+                                {{-- <h5 class="text-success">2.00% increased</h5> --}}
+                            </div>
+                        </div> 
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
