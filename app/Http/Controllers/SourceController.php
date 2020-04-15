02ed56278cd5eaf0521bf75f0d4fe1e7abfcfc61
Isaac Batista <disa.indus@gmail.com>
2020-04-15 14:49:47 -0500

    migraciones 1.17

--- app/Http/Controllers/SourceController.php
+++ app/Http/Controllers/SourceController.php
@@ -185,7 +185,7 @@ class SourceController extends Controller
     }
 
     public function migrationSources(){
-        $oldSources = DB::connection('opemediosold')->table('fuente')->where('id_fuente', '>', 5430)->get();
+        $oldSources = DB::connection('opemediosold')->table('fuente')->where('id_fuente', '>', 5900)->get();
         $count = array();
         $count['fuentes'] = 0;
         $count['secciones'] = 0;
