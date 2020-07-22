5199143ed8f2e2a3ff44d73589f7d3a8e3afe2be
Isaac Batista <disa.indus@gmail.com>
2020-07-22 00:39:49 -0500

    update template for notification new note

--- resources/views/mail/noticeNews.blade.php
+++ resources/views/mail/noticeNews.blade.php
@@ -163,49 +163,12 @@
         </tr>
     </table>
     <!-- pre-header end -->
-    <!-- header -->
-    <table border="0" width="100%" cellpadding="0" cellspacing="0" bgcolor="ffffff">
-        <tr>
-            <td align="center">
-                <table border="0" align="center" width="590" cellpadding="0" cellspacing="0" class="container590">
-                    <tr>
-                        <td height="25" style="font-size: 25px; line-height: 25px;">&nbsp;</td>
-                    </tr>
-                    <tr>
-                        <td align="center">
-                            <table border="0" align="center" width="590" cellpadding="0" cellspacing="0" class="container590">
-                                <tr>
-                                    <td align="center" height="70" style="height:70px;">
-                                        <a href="{{ route('home') }}" style="display: block; border-style: none !important; border: 0 !important;">
-                                            <img width="100" border="0" style="display: block; width: 100px;" src="http://staging.opemedios.com.mx/images/logo.png" alt="Opemedios" />
-                                        </a>
-                                    </td>
-                                </tr>
-                            </table>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td height="25" style="font-size: 25px; line-height: 25px;">&nbsp;</td>
-                    </tr>
-                </table>
-            </td>
-        </tr>
-    </table>
-    <!-- end header -->
-
     <!-- big image section -->
     <table border="0" width="100%" cellpadding="0" cellspacing="0" bgcolor="ffffff" class="bg_color">
 
         <tr>
             <td align="center">
                 <table border="0" align="center" width="590" cellpadding="0" cellspacing="0" class="container590">
-                    <tr>
-                        <td align="center" class="section-img" style="background-color: {{ $news->mean->getColor() }};  -webkit-border-radius: 6px; -moz-border-radius: 6px; border-radius: 6px;">
-                            <a href="{{ route('front.detail.news', ['qry' => Illuminate\Support\Facades\Crypt::encryptString("{$news->id}-{$news->title}-{$theme->company->id}")]) }}" style=" border-style: none !important; display: block; border: 0 !important;">
-                                <img src="{{ asset("images/mails/{$news->mean->icon}.svg") }}" style="display: block; max-width: 35%;" width="590" border="0" alt="{{ $news->title }}" />
-                            </a>
-                        </td>
-                    </tr>
                     <tr>
                         <td height="20" style="font-size: 20px; line-height: 20px;">&nbsp;</td>
                     </tr>
@@ -315,96 +278,10 @@
         <tr>
             <td height="40" style="font-size: 40px; line-height: 40px;">&nbsp;</td>
         </tr>
-
-        {{-- <tr>
-            <td height="60" style="border-top: 1px solid #e0e0e0;font-size: 60px; line-height: 60px;">&nbsp;</td>
-        </tr> --}}
-
-        {{-- <tr>
-            <td align="center">
-                <table border="0" align="center" width="590" cellpadding="0" cellspacing="0" class="container590 bg_color">
-
-                    <tr>
-                        <td>
-                            <table border="0" width="300" align="left" cellpadding="0" cellspacing="0" style="border-collapse:collapse; mso-table-lspace:0pt; mso-table-rspace:0pt;" class="container590">
-
-                                <tr>
-                                    <!-- logo -->
-                                    <td align="left">
-                                        <a href="" style="display: block; border-style: none !important; border: 0 !important;"><img width="80" border="0" style="display: block; width: 80px;" src="https://mdbootstrap.com/img/logo/mdb-email.png" alt="" /></a>
-                                    </td>
-                                </tr>
-
-                                <tr>
-                                    <td height="25" style="font-size: 25px; line-height: 25px;">&nbsp;</td>
-                                </tr>
-
-                                <tr>
-                                    <td align="left" style="color: #888888; font-size: 14px; font-family: 'Work Sans', Calibri, sans-serif; line-height: 23px;" class="text_color">
-                                        <div style="color: #333333; font-size: 14px; font-family: 'Work Sans', Calibri, sans-serif; font-weight: 600; mso-line-height-rule: exactly; line-height: 23px;">
-
-                                            Email us: <br/> <a href="mailto:" style="color: #888888; font-size: 14px; font-family: 'Hind Siliguri', Calibri, Sans-serif; font-weight: 400;">contact@mdbootstrap.com</a>
-
-                                        </div>
-                                    </td>
-                                </tr>
-
-                            </table>
-
-                            <table border="0" width="2" align="left" cellpadding="0" cellspacing="0" style="border-collapse:collapse; mso-table-lspace:0pt; mso-table-rspace:0pt;" class="container590">
-                                <tr>
-                                    <td width="2" height="10" style="font-size: 10px; line-height: 10px;"></td>
-                                </tr>
-                            </table>
-
-                            <table border="0" width="200" align="right" cellpadding="0" cellspacing="0" style="border-collapse:collapse; mso-table-lspace:0pt; mso-table-rspace:0pt;" class="container590">
-
-                                <tr>
-                                    <td class="hide" height="45" style="font-size: 45px; line-height: 45px;">&nbsp;</td>
-                                </tr>
-
-
-
-                                <tr>
-                                    <td height="15" style="font-size: 15px; line-height: 15px;">&nbsp;</td>
-                                </tr>
-
-                                <tr>
-                                    <td>
-                                        <table border="0" align="right" cellpadding="0" cellspacing="0">
-                                            <tr>
-                                                <td>
-                                                    <a href="https://www.facebook.com/mdbootstrap" style="display: block; border-style: none !important; border: 0 !important;"><img width="24" border="0" style="display: block;" src="http://i.imgur.com/Qc3zTxn.png" alt=""></a>
-                                                </td>
-                                                <td>&nbsp;&nbsp;&nbsp;&nbsp;</td>
-                                                <td>
-                                                    <a href="https://twitter.com/MDBootstrap" style="display: block; border-style: none !important; border: 0 !important;"><img width="24" border="0" style="display: block;" src="http://i.imgur.com/RBRORq1.png" alt=""></a>
-                                                </td>
-                                                <td>&nbsp;&nbsp;&nbsp;&nbsp;</td>
-                                                <td>
-                                                    <a href="https://plus.google.com/u/0/b/107863090883699620484/107863090883699620484/posts" style="display: block; border-style: none !important; border: 0 !important;"><img width="24" border="0" style="display: block;" src="http://i.imgur.com/Wji3af6.png" alt=""></a>
-                                                </td>
-                                            </tr>
-                                        </table>
-                                    </td>
-                                </tr>
-
-                            </table>
-                        </td>
-                    </tr>
-                </table>
-            </td>
-        </tr>
-
-        <tr>
-            <td height="60" style="font-size: 60px; line-height: 60px;">&nbsp;</td>
-        </tr> --}}
-
     </table>
     <!-- end section -->
     <!-- footer ====== -->
     <table border="0" width="100%" cellpadding="0" cellspacing="0" bgcolor="f4f4f4">
-
         <tr>
             <td height="25" style="font-size: 25px; line-height: 25px;">&nbsp;</td>
         </tr>
@@ -414,6 +291,11 @@
                 <table border="0" align="center" width="590" cellpadding="0" cellspacing="0" class="container590">
 
                     <tr>
+                        <td align="left" height="70" style="height:70px;">
+                            <a href="{{ route('home') }}" style="display: block; border-style: none !important; border: 0 !important;">
+                                <img width="100" border="0" style="display: block; width: 100px;" src="http://staging.opemedios.com.mx/images/logo.png" alt="Opemedios" />
+                            </a>
+                        </td>
                         <td>
                             <table border="0" align="center" cellpadding="0" cellspacing="0" style="border-collapse:collapse; mso-table-lspace:0pt; mso-table-rspace:0pt;" class="container590">
                                 <tr>
