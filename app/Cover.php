6f6f181b8825eb50a690c242851b71b5ea124230
Isaac Batista <disa.indus@gmail.com>
2020-07-24 20:48:04 -0500

    Comments in files

--- app/Cover.php
+++ app/Cover.php
@@ -1,7 +1,23 @@
 <?php
 
 namespace App;
-
+/**
+  *-------------------------------------------------------------------------------------
+  * Developer Information
+  *-------------------------------------------------------------------------------------
+  * @author Isaac Daniel Batista <daniel@danielbat.com>
+  * @link https://danielbat.com Web Autor's site
+  * @see https://twitter.com/codeisaac <@codeisaac>
+  * @copyright 2020
+  * @version 1.0.0
+  * @package App\
+  * Type: Model
+  * Description: Description
+  *
+  * For the full copyright and license information, please view the LICENSE
+  * file that was distributed with this source code.
+  */
+        
 use App\Source;
 use Illuminate\Database\Eloquent\Model;
 
