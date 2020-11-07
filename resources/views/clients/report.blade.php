34a386c088d7d384259ce601d4fd30f063246780
ppgomez <elbis666@gmail.com>
2020-11-06 20:27:10 -0600

    AJUSTE 2 columnas formulario Reporte

--- resources/views/clients/report.blade.php
+++ resources/views/clients/report.blade.php
@@ -4,10 +4,11 @@
     @include('components.clientHeading')
     <!--Page Content -->
     <div class="uk-padding op-content-mt main-content">
-        <div class="uk-width-large uk-padding-large uk-padding-remove-horizontal reporte-container">
+        <div class="uk-width-2xlarge uk-padding-large uk-padding-remove-horizontal reporte-container">
             <h1 class="page-header">Reporte <span class="tema-actual"></span></h1>
             <br>
             <form action="{{ route('client.report', ['company' => session()->get('slug_company')]) }}" method="POST">
+                <div class="uk-child-width-1-1 uk-child-width-1-2@s uk-child-width-1-2@m" uk-grid>
                 @csrf
                 <input type="hidden" name="company_id" value="{{ Auth::user()->company()->id }}">
                 <div class="uk-margin">
@@ -47,6 +48,7 @@
                     <select class="form-control uk-select" name="mean_id" id="">
                         <option value="default">** Todos **</option>
                     </select>
+                </div>
                 </div>
                 <div class="uk-margin">
                     <input class="btn btn-action uk-button uk-button-large uk-button-default uk-box-shadow-medium" type="submit" value="Generar">
