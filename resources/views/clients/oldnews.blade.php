943cdd57c37d2143ddf81bade27df9c1e1dcfc01
ppgomez <elbis666@gmail.com>
2020-11-10 23:41:40 -0600

    ajustes blades: listado  old news y old new

--- resources/views/clients/oldnews.blade.php
+++ resources/views/clients/oldnews.blade.php
@@ -3,8 +3,12 @@
 @section('content')
     <!-- Page Content -->
     {{--@include('components.clientHeading')--}}
-    <div class="uk-container op-content-mt" id="list-news">
-        @include('components.listOldNews')
+    <div class="uk-padding op-content-mt main-content" id="list-news">
+        <div class="uk-padding uk-padding-large uk-padding-remove-horizontal">
+        	<div class="uk-padding uk-padding-remove-horizontal">
+        		@include('components.listOldNews')
+        	</div>
+    	</div>
     </div>
     <!-- /.container -->
 @endsection
