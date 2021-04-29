4a34c583997f34c3a8bf948465422afbef68c68e
Isaac Daniel Batista <isaacbatista@wolfsellers.com>
2021-04-29 14:33:05 -0500

    add headers with developer info

--- app/Exports/ReportsExport.php
+++ app/Exports/ReportsExport.php
@@ -1,4 +1,20 @@
 <?php
+/**
+  *-------------------------------------------------------------------------------------
+  * Developer Information
+  *-------------------------------------------------------------------------------------
+  * @author Isaac Daniel Batista <daniel@danielbat.com>
+  * @link https://danielbat.com Web Autor's site
+  * @see https://twitter.com/codeisaac <@codeisaac>
+  * @copyright 2021
+  * @version 1.0.0
+  * @package App\
+  * Type: Export
+  * Description: Description
+  *
+  * For the full copyright and license information, please view the LICENSE
+  * file that was distributed with this source code.
+  */
 
 namespace App\Exports;
 
