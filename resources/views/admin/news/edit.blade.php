070d5983d0f31cdf61243bb446408d21c47e79ac
Isaac Daniel Batista <isaacbatista@wolfsellers.com>
2021-05-03 10:21:32 -0500

    fis on select section on edit note

--- resources/views/admin/news/edit.blade.php
+++ resources/views/admin/news/edit.blade.php
@@ -51,7 +51,9 @@
                     <label class="col-sm-2 col-md-1 col-lg-1 col-form-label" for="select-seccion">{{ __('Secciones') }}: <span class="text-danger">*</span></label>
                     <div class="col-sm-10 col-md-11 col-lg-11">
                         <select class="form-control" id="select-seccion" name="section_id">
-                            <option value="{{ $note->section_id ?? '' }}">{{ $note->section->name ?? 'N/E' }}</option>
+                            @foreach (App\Section::all() as $section)
+                                <option value="{{ $section->id ?? '' }}" {{ (old('section_id', $note->section_id) == $section->id ? 'selected' : '' ) }}>{{ $note->section->name ?? 'N/E' }}</option>
+                            @endforeach
                         </select>
                         @error('section_id')
                             <label class="text-danger">
