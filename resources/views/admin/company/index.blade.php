b09e713ab3539e693a2bd5a1081f369ba39fb460
Isaac Daniel Batista <isaacbatista@wolfsellers.com>
2021-05-16 23:31:02 -0500

    filters from company already

--- resources/views/admin/company/index.blade.php
+++ resources/views/admin/company/index.blade.php
@@ -6,85 +6,85 @@
         {{ session('status') }}
     </div>
 @endif
-<div class="col-sm-8 col-md-9 col-lg-10">
-  <div class="panel">
-    <div class="panel-heading">
-      <div class="row">
-          <div class="col-lg-6 col-md-8 col-sm-6 col-xs-12">
-            <h4 class="panel-title" style="padding: 12px 0;">Administrador de empresas</h4>  
-          </div>
-          <div class="col-lg-6 col-md-4 col-sm-6 col-xs-12 text-right">
-              <a href="{{ route('company.create') }}" class="btn btn-success btn-quirk"><i class="fa fa-plus-circle"></i> Nueva empresa</a>
-          </div>
-      </div>
-    </div>
-    <div class="panel-body">
-        <div class="table-responsive">
-            <table class="table table-bordered table-inverse table-striped nomargin" id="table-company-list">
-              <thead>
-                <tr>
-                  <th class="text-center">
-                    <label class="ckbox">
-                      <input type="checkbox"><span></span>
-                    </label>
-                  </th>
-                  <th class="text-center">#</th>
-                  <th class="text-center">Nombre</th>
-                  <th class="text-center">Giro</th>
-                  <th class="text-center">Acciones</th>
-                </tr>
-              </thead>
-              <tbody>
-                @foreach($companies as $company)
-                    <tr>
-                      <td class="text-center">
-                        <label class="ckbox">
-                          <input type="checkbox"><span></span>
-                        </label>
-                      </td>
-                      <td class="text-center" >{{ ($companies->currentPage() - 1) * $companies->perPage() + $loop->iteration }}</td>
-                      <td class="text-left" >{{ $company->name }}</td>
-                      <td class="text-left">{{ $company->turn->name }}</td>
-                      <td class="table-options">
-                          <li><a href="{{ route('company.show', ['id' => $company->id]) }}"><i class="fa fa-eye"></i></a></li>
-                          <li><a href="{{ route('admin.company.delete', ['id' => $company->id]) }}" class="btn-delete-company" data-name="{{ $company->name }}"><i class="fa fa-trash"></i></a></li>
-                      </td>
-                    </tr>
-                @endforeach
-              </tbody>
-            </table>
-            {!! $companies->links() !!}        
+<div class="col-md-12 people-list">
+    <div class="people-options clearfix"> <!-- filter-options -->
+        <div class="btn-toolbar">
+            <form action="{{ route('companies') }}" method="GET">
+                <div class="row">
+                    <div class="col-md-2 form-group">
+                        <label for="input-company-name" class="text-muted">Nombre</label>
+                        <input type="text" name="name" class="form-control" id="input-company-name" value="{{ request()->get('name') }}">
+                    </div>
+                    <div class="col-md-2 form-group">
+                        <label for="select-company-turn" class="text-muted">Giro</label>
+                        <select class="form-control" name="turn">
+                            <option value="">Giro</option>
+                            @foreach(App\Turn::all() as $turn)
+                                <option value="{{ $turn->id }}" {{ request()->get('turn') == $turn->id ? 'selected' : '' }}>{{ $turn->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="col-md-2 form-group">
+                        <label for="select-user-page" class="text-muted">Por p&aacute;gina</label>
+                        <select class="form-control" name="paginate">
+                            <option value="5" {{ $paginate == 5 ? 'selected' : '' }}>5</option>
+                            <option value="10" {{ $paginate == 10 ? 'selected' : '' }}>10</option>
+                            <option value="25" {{ $paginate == 25 ? 'selected' : '' }}>25</option>
+                            <option value="50" {{ $paginate == 50 ? 'selected' : '' }}>50</option>
+                            <option value="100" {{ $paginate == 100 ? 'selected' : '' }}>100</option>
+                        </select>
+                    </div>
+                    <div class="col-md-4 form-group" style="margin-top: 20px">
+                        <button class="btn btn-primary btn-lg"> Buscar</button>
+                        @if(request()->has('name') || request()->has('turn') )
+                            <a href="{{ route('companies') }}" class="btn btn-warning ml-2" style="margin-left: .8em"> Limpiar filtros </a>
+                        @endif
+                    </div>
+                </div>
+            </form>
+        </div>
+        <span id="span-count-info" class="people-count pull-right">Mostrando <strong id="num-rows-info">{{ $companies->count() }} de {{ $companies->total() }}</strong> noticias</span>
+    </div><!-- filter-options -->
+    <div class="panel">
+        <div class="panel-heading">
+            <div class="row">
+                <div class="col-lg-6 col-md-8 col-sm-6 col-xs-12">
+                    <h4 class="panel-title" style="padding: 12px 0;">Administrador de empresas</h4>  
+                </div>
+                <div class="col-lg-6 col-md-4 col-sm-6 col-xs-12 text-right">
+                    <a href="{{ route('company.create') }}" class="btn btn-success btn-quirk"><i class="fa fa-plus-circle"></i> Nueva empresa</a>
+                </div>
+            </div>
+        </div>
+        <div class="panel-body">
+            <div class="table-responsive">
+                <table class="table table-bordered table-primary table-striped nomargin" id="table-company-list">
+                    <thead>
+                        <tr>
+                            <th class="text-center">#</th>
+                            <th class="text-center">Nombre</th>
+                            <th class="text-center">Giro</th>
+                            <th class="text-center">Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($companies as $company)
+                            <tr>
+                                <td class="text-center" >{{ ($companies->currentPage() - 1) * $companies->perPage() + $loop->iteration }}</td>
+                                <td class="text-left" >{{ $company->name }}</td>
+                                <td class="text-left">{{ $company->turn->name }}</td>
+                                    <td class="table-options">
+                                        <li><a href="{{ route('company.show', ['id' => $company->id]) }}"><i class="fa fa-eye"></i></a></li>
+                                        <li><a href="{{ route('admin.company.delete', ['id' => $company->id]) }}" class="btn-delete-company" data-name="{{ $company->name }}"><i class="fa fa-trash"></i></a></li>
+                                    </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                {!! $companies->links() !!}        
       </div><!-- table-responsive -->
     </div>
   </div><!-- panel -->
-</div>
-<div class="col-sm-4 col-md-3 col-lg-2">
-    <div class="panel">
-        <div class="panel-heading">
-            <h4 class="panel-title">{{ __('Filtrar Empresas') }}</h4>
-        </div>
-        <div class="panel-body">
-            <form action="{{ route('companies') }}" method="GET">
-                <div class="form-group">
-                    <label class="control-label center-block">{{ __('Buscar por nombre') }}</label>
-                    <input type="text" name="name" class="form-control" placeholder="{{ __('Buscar por nombre') }}" value="{{ request()->get('name') }}">
-                </div>
-                <div class="form-group">
-                    <label class="control-label center-block">{{ __('Giro') }}</label>
-                    <select name="turn" class="form-control">
-                        <option value="">{{ __('Filtrar por giro') }}</option>
-                        @foreach($turns as $turn)
-                            @if($turn->name == 'disable')
-                                @continue
-                            @endif
-                            <option value="{{ $turn->id }}" {{ request()->get('turn') == $turn->id ? 'selected' : '' }}>{{ $turn->name }}</option>
-                        @endforeach
-                    </select>
-                </div>
-                <input type="submit" class="btn btn-success btn-quirk btn-block" value="{{ __('Filtrar') }}">
-            </form>
-        </div>
-    </div><!-- panel -->
 </div>
 @endsection
 @section('scripts')
