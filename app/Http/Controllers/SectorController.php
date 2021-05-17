23ac10c8691259683be8688043a5488b18b52f00
Isaac Daniel Batista <isaacbatista@wolfsellers.com>
2021-05-17 00:09:00 -0500

    filters from turns already and breadcrumb

--- app/Http/Controllers/SectorController.php
+++ app/Http/Controllers/SectorController.php
@@ -30,11 +30,17 @@ class SectorController extends Controller
      */
     public function index(Request $request)
     {
+        $breadcrumb = array();
+        array_push($breadcrumb,['label' => 'Sectores']);
+
+        $paginate = $request->has('paginate') ? $request->input('paginate') : 25;
+
         $sectors = Sector::name($request->get('name'))
             ->orderBy('id', 'DESC')
-            ->paginate(25)
+            ->paginate($paginate)
             ->appends('name', request('name'));
-        return view('admin.sector.index', compact('sectors'));
+
+        return view('admin.sector.index', compact('sectors', 'breadcrumb', 'paginate'));
     }
 
     /**
@@ -44,7 +50,11 @@ class SectorController extends Controller
      */
     public function create()
     {
-        return view('admin.sector.create');
+        $breadcrumb = array();
+        array_push($breadcrumb,['label' => 'Sectores', 'url' => route('admin.sectors')]);
+        array_push($breadcrumb,['label' => 'Nuevo Sector']);
+
+        return view('admin.sector.create', compact('breadcrumb'));
     }
 
     /**
@@ -72,7 +82,11 @@ class SectorController extends Controller
     {
         $sector = Sector::findOrFail($id);
 
-        return view('admin.sector.edit', compact('sector'));
+        $breadcrumb = array();
+        array_push($breadcrumb,['label' => 'Sectores', 'url' => route('admin.sectors')]);
+        array_push($breadcrumb,['label' => "Editar {$sector->name}"]);
+
+        return view('admin.sector.edit', compact('sector', 'breadcrumb'));
     }
 
     /**
