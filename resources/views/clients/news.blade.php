8ba4d660a19d54d4198a4bb854813b77c63525ed
Isaac Daniel Batista <isaacbatista@wolfsellers.com>
2021-06-02 00:45:14 -0500

    see if it has notes the subject

--- resources/views/clients/news.blade.php
+++ resources/views/clients/news.blade.php
@@ -19,7 +19,8 @@
         <div class="op-content-mt main-content js-temas uk-padding uk-padding-remove-bottom" style="background: #fff;">
             @foreach($company->themes as $theme)
             <div class="row theme{{ $theme->id }}" id="list-new">
-                    <h2 id="theme{{ $theme->id }}">{{ $theme->name }} <small class="count" id="count-{{ $theme->id }}"></small></h2>
+                @if($theme->assignedNews()->count())
+                    <h2 id="theme{{ $theme->id }}" >{{ $theme->name }} <small class="count" id="count-{{ $theme->id }}"></small></h2>
                     @if($company->assignedNews->count() > 0)
                     @php
                     $contadorEntradas = 0;
@@ -63,6 +64,7 @@
                     @else
                         <strong>No hay Noticias que mostrar</strong>
                     @endif
+                @endif
             </div>
             @endforeach
         </div>
