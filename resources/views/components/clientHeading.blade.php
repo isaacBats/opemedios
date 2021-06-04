f73c22b170fa4a33298d666be10151086c40298a
Isaac Daniel Batista <isaacbatista@wolfsellers.com>
2021-06-03 23:34:19 -0500

    count from notes on client view

--- resources/views/components/clientHeading.blade.php
+++ resources/views/components/clientHeading.blade.php
@@ -7,8 +7,8 @@
     <div>
         <h1 style="color: #fff; font-size: 1.5em;">{{ "Bienvenido " . $company->name }}</h1>
         <div uk-grid>
-            <p>Noticias de hoy: <strong>{{ $company->assignedNews->where('created_at', Carbon\Carbon::today()->format('Y-m-d'))->count() }}</strong></p>
-            <p>Noticias del mes: <strong>{{ $company->assignedNews->where('created_at', Carbon\Carbon::today()->format('Y-m'))->count() }}</strong></p>
+            <p>Noticias de hoy: <strong>{{ $company->assignedNews()->whereDate('created_at', Carbon\Carbon::today()->format('Y-m-d'))->count() }}</strong></p>
+            <p>Noticias del mes: <strong>{{ $company->assignedNews()->whereYear('created_at', Carbon\Carbon::today()->format('Y'))->whereMonth('created_at', Carbon\Carbon::today()->format('m'))->count() }}</strong></p>
             <p>Total: <strong>{{ $company->assignedNews->count() }}</strong></p>
             <div id="search">
                 @include('components.search-bar')
