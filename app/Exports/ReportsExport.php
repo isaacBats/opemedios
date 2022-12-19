f6600e52294d05ea6be5d9bd955526a41b1bf12a
Josué Martínez <37030363+jmartinezvision@users.noreply.github.com>
2022-12-19 04:34:33 -0600

    Graficos en excel

--- app/Exports/ReportsExport.php
+++ app/Exports/ReportsExport.php
@@ -239,7 +239,11 @@ class ReportsExport implements FromQuery, WithCharts, WithMapping, WithHeadings,
 
     public function charts() {
 
-        $dt = ['B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','X','Y','Z','AA','AB','AC','AD','AE','AF','AG','AH','AI','AJ','AK','AL','AM','AN','AO','AP','AQ','AR','AS','AT','AU','AV','AX','AY','AZ','BA','BB','BC','BD','BE','BF','BG','BH','BI','BJ','BK','BL','BM','BN','BO','BP','BQ','BR','BS','BT','BU','BV','BX','BY','BZ','CA','CB','CC','CD','CE','CF','CG','CH','CI','CJ'];
+        $dt = [
+            'B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','X','Y','Z','AA','AB','AC','AD','AE','AF','AG','AH','AI','AJ','AK','AL','AM','AN','AO','AP','AQ','AR','AS','AT','AU','AV','AX','AY','AZ',
+            'BA','BB','BC','BD','BE','BF','BG','BH','BI','BJ','BK','BL','BM','BN','BO','BP','BQ','BR','BS','BT','BU','BV','BX','BY','BZ',
+            'CA','CB','CC','CD','CE','CF','CG','CH','CI','CJ','CK','CL','CM','CN','CO','CP','CQ','CR','CS','CT','CU','CV','CX','CY','CZ',
+            'DA','DB','DC','DD','DE','DF','DG','DH','DI','DJ','DK','DL','DM'];
         
                 //	Set the Labels for each data series we want to plot
                 //		Datatype
