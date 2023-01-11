92d05119766fd50b2096d0bd954dddf79db7f3aa
Isaac Batista <disa.indus@gmail.com>
2023-01-10 19:08:32 -0600

    Update ReportsExport.php
    add more letters

--- app/Exports/ReportsExport.php
+++ app/Exports/ReportsExport.php
@@ -234,7 +234,7 @@ class ReportsExport implements FromQuery, WithCharts, WithMapping, WithHeadings,
             'B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','X','Y','Z','AA','AB','AC','AD','AE','AF','AG','AH','AI','AJ','AK','AL','AM','AN','AO','AP','AQ','AR','AS','AT','AU','AV','AX','AY','AZ',
             'BA','BB','BC','BD','BE','BF','BG','BH','BI','BJ','BK','BL','BM','BN','BO','BP','BQ','BR','BS','BT','BU','BV','BX','BY','BZ',
             'CA','CB','CC','CD','CE','CF','CG','CH','CI','CJ','CK','CL','CM','CN','CO','CP','CQ','CR','CS','CT','CU','CV','CX','CY','CZ',
-            'DA','DB','DC','DD','DE','DF','DG','DH','DI','DJ','DK','DL','DM'];
+            'DA','DB','DC','DD','DE','DF','DG','DH','DI','DJ','DK','DL','DM','DN','DO','DP','DQ','DR','DS','DT','DU','DV','DX','DY','DZ'];
     
         /* CHART LINE */                    
             foreach($this->themes as $key => $itm)
