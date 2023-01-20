67b87c704ea1613a96eceab04fc2227721235662
Isaac Batista <disa.indus@gmail.com>
2023-01-20 17:13:53 -0600

    restart query for themes asigned

--- app/Exports/ReportsExport.php
+++ app/Exports/ReportsExport.php
@@ -105,7 +105,18 @@ class ReportsExport implements FromQuery, WithCharts, WithMapping, WithHeadings,
 
         $where = '';
 
-        $this->themes = $this->client->themes;
+        $qry = "select themes.id, themes.name
+        from assigned_news
+        inner join news on assigned_news.news_id = news.id
+        inner join themes on assigned_news.theme_id = themes.id
+        where news.id in (" . str_replace(']', '', str_replace('[', '', $notesIds)) . ")
+        AND date(news.created_at) BETWEEN '". $from->format('Y-m-d') ."' AND '" . $to->format('Y-m-d') ."'
+        group by themes.id, themes.name
+        order by name desc";
+
+        $themes = DB::select($qry);
+        $this->themes = $themes;
+
         $this->columnas_generadas = $this->generaColumnasExcel();
         $period = CarbonPeriod::create($from, $to);
 
