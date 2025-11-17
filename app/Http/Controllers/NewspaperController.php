f24da51126dfbd12974cda38f0305be2bd0fc4ac
Josue Martinez <chayo_916@hotmail.com>
2025-11-17 04:09:10 -0600

    ModuloNewspaper datos repetidos

--- app/Http/Controllers/NewspaperController.php
+++ app/Http/Controllers/NewspaperController.php
@@ -8,13 +8,24 @@ class NewspaperController extends Controller
 {
 
     public function index(Request $request) {
-        $newspapers = NewspaperFile::orderBy('id', 'desc')->paginate(10);
+        $newspapers = NewspaperFile::groupBy(['newspaper', 'date'])->orderBy('id', 'desc')->paginate(10);
 
         return view('admin.newspaper.index', compact('newspapers'));
     }
 
     public function indexGuest(Request $request) {
-        $newspapers = NewspaperFile::orderBy('id', 'desc')->paginate(30);
+        //$newspapers = NewspaperFile::groupBy(['newspaper', 'date'])->orderBy('id', 'desc')->paginate(30);
+        $newspapers = NewspaperFile::select(
+            'newspaper',
+            'date',
+            // Usamos MAX() para obtener el ID más alto dentro de cada grupo
+            \DB::raw('MAX(id) as latest_id'), 
+            // Y la función apropiada para obtener el 'file' asociado
+            \DB::raw('SUBSTRING_INDEX(GROUP_CONCAT(file ORDER BY id DESC), ",", 1) as latest_file')
+        )
+        ->groupBy(['newspaper', 'date'])
+        ->orderBy('latest_id', 'desc')
+        ->paginate(10);
 
         return view('admin.newspaper.index_guest', compact('newspapers'));
     }
